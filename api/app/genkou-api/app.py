bd78abaa30a7611978f77568c0609e2e19b947b3
miyanagas <zs0h8po2t7ad@gmail.com>
2024-11-01 15:09:56 +0900

    タイマーデータを新規作成する機能を実装

--- api/app/genkou-api/app.py
+++ api/app/genkou-api/app.py
@@ -6,6 +6,7 @@ from sqlmodel import select
 
 from .database import create_db_and_tables, SessionDep
 from .models import Script, ScriptCreate, ScriptPublic, ScriptUpdate
+from .models import TimerInterval, TimerTreePath, TimerIntervalCreate, TimerIntervalPublic, TimerIntervalUpdate
 
 app = FastAPI()
 
@@ -47,3 +48,53 @@ def update_script(script_id: uuid.UUID, script: ScriptUpdate, session: SessionDe
     session.commit()
     session.refresh(db_script)
     return db_script
+
+def add_timer(timer_interval: dict, script_id: uuid.UUID, session: SessionDep, ancestor_id: uuid.UUID=None, depth: int=0):
+    timer = timer_interval["timer"]
+    db_timer = TimerInterval.model_validate(TimerInterval(timer=timer, script_id=script_id))
+    session.add(db_timer)
+    session.commit()
+    session.refresh(db_timer)
+
+    if ancestor_id is not None:
+        db_tree_path = TimerTreePath(ancestor_id=ancestor_id, descendant_id=db_timer.id, depth=depth)
+        session.add(db_tree_path)
+        session.commit()
+
+    db_tree_path = TimerTreePath(ancestor_id=db_timer.id, descendant_id=db_timer.id, depth=0)
+    session.add(db_tree_path)
+    session.commit()
+
+    if timer_interval["interval"] == []:
+        return
+    for interval in timer_interval["interval"]:
+        add_timer(interval, script_id, session, ancestor_id=db_timer.id, depth=depth+1)
+
+def get_timer_interval(script_id: uuid.UUID, session: SessionDep):
+    root_timer = session.exec(select(TimerInterval).filter(TimerInterval.script_id == script_id)).first()
+    if not root_timer:
+        return None
+
+    def get_interval(timer_id: uuid.UUID):
+        timer_intervals =[]
+        intervals = session.exec(select(TimerInterval).join(TimerTreePath, TimerTreePath.descendant_id == TimerInterval.id).filter(TimerTreePath.ancestor_id == timer_id, TimerTreePath.depth == 1)).all()
+
+        for timer_interval in intervals:
+            timer_intervals.append({
+                "timer": timer_interval.timer,
+                "interval": get_interval(timer_interval.id)
+            })
+
+        return timer_intervals
+
+    return {
+        "timer": root_timer.timer,
+        "interval": get_interval(root_timer.id)
+    }
+
+# create a timer interval api
+@app.post("/timer-intervals/{script_id}", response_model=TimerIntervalPublic)
+def create_timer_interval(script_id: uuid.UUID, timer_interval: TimerIntervalCreate, session: SessionDep):
+    timer_interval_data = timer_interval.model_dump()
+    add_timer(timer_interval_data, script_id, session)
+    return TimerIntervalPublic(**get_timer_interval(script_id, session))
