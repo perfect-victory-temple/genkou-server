5c8fe47195ac16c38961f77326e0e2f48e0f705f
miyanagas <zs0h8po2t7ad@gmail.com>
2024-11-01 15:09:32 +0900

    タイマーデータのモデリング

--- api/app/genkou-api/models.py
+++ api/app/genkou-api/models.py
@@ -26,3 +26,27 @@ class ScriptUpdate(ScriptBase):
     title: str | None = None
     content: str | None = None
     timer: int | None = None
+
+# timer_interval model
+class TimerIntervalBase(SQLModel):
+    timer: int
+
+class TimerInterval(TimerIntervalBase, table=True):
+    script_id: uuid.UUID
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
+class TimerTreePath(SQLModel, table=True):
+    ancestor_id: uuid.UUID = Field(primary_key=True)
+    descendant_id: uuid.UUID = Field(primary_key=True)
+    depth: int
+
+class TimerIntervalCreate(TimerIntervalBase):
+    interval: list['TimerIntervalCreate'] = []
+
+class TimerIntervalPublic(TimerIntervalBase):
+    timer: int | None
+    interval: list['TimerIntervalPublic'] = []
+
+class TimerIntervalUpdate(TimerIntervalBase):
+    timer: int | None = None
+    interval: list['TimerIntervalUpdate'] = []
